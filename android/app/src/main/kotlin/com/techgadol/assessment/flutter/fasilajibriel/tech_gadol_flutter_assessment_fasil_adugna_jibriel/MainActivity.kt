35d873f2cf7a39a75ffa6d1fc3cc1e94a051eb0f
Fasil Jibriel <fasilajibriel@gmail.com>
2026-03-05 22:39:51 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/techgadol/assessment/flutter/fasilajibriel/tech_gadol_flutter_assessment_fasil_adugna_jibriel/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.techgadol.assessment.flutter.fasilajibriel.tech_gadol_flutter_assessment_fasil_adugna_jibriel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
